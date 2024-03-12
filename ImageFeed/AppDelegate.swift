6363633a7ef172d1add91e3357a56cf0cc1dbf85
riamaks <ria.maks.trw@gmail.com>
2024-03-12 22:31:45 +0300

    Sprint 08

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  ImageFeed
-//
-//  Created by Мария Малышева on 19.02.2024.
-//
-
 import UIKit
 
 @main
--- ImageFeed/SceneDelegate.swift
+++ ImageFeed/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  ImageFeed
-//
-//  Created by Мария Малышева on 19.02.2024.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
